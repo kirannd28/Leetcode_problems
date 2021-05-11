9afcc2a88729435cddf0cfcca1ddfd3476bfc59d
Kiran ND <kirannd89@gmail.com>
2021-05-11 09:00:56 +0100

    making it a template

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,23 @@
+// Source : 
+// Author : Kiran <kirannd89@gmail.com
+// Date   : 
+
+/********************************************************************************** 
+* 
+*   < copy description here >
+* 
+*               
+**********************************************************************************/
+
+#include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-
-    cout << "Hello World" << endl;
+int main()
+{
     return 0;
 }
